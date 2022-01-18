8c0aa2d18c3e84464780439c4f79c1b2cc012e33
NoyPhilosof <96237612+NoyPhilosof@users.noreply.github.com>
2022-01-18 13:18:59 +0200

    adding pycharm env pt.2

---
+++ main.py
@@ -0,0 +1,20 @@
+import states
+
+states.pic_1()
+print()
+states.pic_2()
+print()
+states.pic_3()
+print()
+states.pic_4()
+print()
+states.pic_5()
+print()
+states.pic_6()
+print()
+states.pic_7()
+
+
+
+
+
